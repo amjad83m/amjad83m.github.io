10429574b140f7566657fcb017d513824a80397b
unknown <amjad83m@hotmail.com>
2017-11-30 01:55:37 +0100

    deleted 2nd and 3rd script

--- scripts/console.js
+++
@@ -1,9 +0,0 @@
-// printing some text to the console
-
-function writeToConsole(userText) {
-    console.log(userText);
-}
-
-writeToConsole('');
-writeToConsole('Hi Laurens, nothing to show here, just the dummy text you requested :)');
-writeToConsole('');
